9884e8411dc3d0e1f132c5731b2a2272ab23b0b2
Jeevan Bennur <bennurjeevan@gmail.com>
2026-04-19 23:45:03 +0530

    Fix frontend-backend CORS and environment configurations for production URL

--- api/main.py
+++ api/main.py
@@ -22,8 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Content-Type", "Accept"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
